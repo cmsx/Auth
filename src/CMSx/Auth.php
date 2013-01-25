f515a9779f7146809464f1461f9f727de0cfbe2d
Igor Osharin <igor@osharin.ru>
2013-01-25 19:15:42 +0400

    Хеширование пароля

--- src/CMSx/Auth.php
+++ src/CMSx/Auth.php
@@ -4,5 +4,9 @@ namespace CMSx;
 
 class Auth
 {
-
+  /** Хеш от логина и пароля */
+  public static function HashPassword($login, $pass)
+  {
+    return hash('whirlpool', hash('sha256', $login) . $pass);
+  }
 }
--- tests/unit/auth.php
+++ tests/unit/auth.php
@@ -6,8 +6,11 @@ use CMSx\Auth;
 
 class AuthTest extends PHPUnit_Framework_TestCase
 {
-  function testMe()
+  function testHash()
   {
+    $exp = '87347c6ba864e8f5d0ad83eeed7a03964269f5ed5bd10aa16d8fb1146cd21ee14160b0b3713ce5258cdeebee8e7e8db4017d8fcd5e5f087d2d5bb75893167794';
+    $this->assertEquals($exp, Auth::HashPassword('user', 'qwerty'), 'Хеш от логина и пароля');
 
+    $this->assertNotEquals($exp, Auth::HashPassword('user2', 'qwerty'), 'Хеш зависит от логина');
   }
 }
