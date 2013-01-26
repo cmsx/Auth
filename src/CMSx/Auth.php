f5f8d9536609b1e85da71f848e6e7ca7bd5f9612
Igor Osharin <igor@osharin.ru>
2013-01-26 04:45:05 +0400

    Обработка и запуск сессии

--- src/CMSx/Auth.php
+++ src/CMSx/Auth.php
@@ -2,11 +2,56 @@
 
 namespace CMSx;
 
+use CMSx\Auth\Exception;
+
 class Auth
 {
+  /** Ошибка - сессия не запущена */
+  const ERR_SESSION = 10;
+
+  protected static $errors = array(
+    self::ERR_SESSION => 'Сессия не запущена'
+  );
+
+  /** Инициализация авторизации */
+  public static function Init($session_autostart = true)
+  {
+    self::CheckSessionStarted($session_autostart);
+  }
+
   /** Хеш от логина и пароля */
   public static function HashPassword($login, $pass)
   {
     return hash('whirlpool', hash('sha256', $login) . $pass);
+  }
+
+  /**
+   * Выброс исключения по коду ошибки
+   * @throws \CMSx\Auth\Exception
+   */
+  public static function ThrowError($code)
+  {
+    throw new Exception(static::GetErrorMsg($code), $code);
+  }
+
+  /** Получение текста ошибки по коду */
+  public static function GetErrorMsg($code)
+  {
+    return isset(static::$errors[$code])
+      ? static::$errors[$code]
+      : 'Неизвестная ошибка';
+  }
+
+  /** Проверка, что сессия запущена */
+  public static function CheckSessionStarted($autostart = false)
+  {
+    $sid = session_id();
+    if (empty($sid)) {
+      if ($autostart) {
+        session_start();
+      } else {
+        static::ThrowError(self::ERR_SESSION);
+      }
+    }
   }
 }
--- tests/unit/auth.php
+++ tests/unit/auth.php
@@ -8,9 +8,27 @@ class AuthTest extends PHPUnit_Framework_TestCase
 {
   function testHash()
   {
-    $exp = '87347c6ba864e8f5d0ad83eeed7a03964269f5ed5bd10aa16d8fb1146cd21ee14160b0b3713ce5258cdeebee8e7e8db4017d8fcd5e5f087d2d5bb75893167794';
+    $exp =
+      '87347c6ba864e8f5d0ad83eeed7a03964269f5ed5bd10aa16d8fb1146cd21ee14160b0b3713ce5258cdeebee8e7e8db4017d8fcd5e5f087d2d5bb75893167794';
     $this->assertEquals($exp, Auth::HashPassword('user', 'qwerty'), 'Хеш от логина и пароля');
 
     $this->assertNotEquals($exp, Auth::HashPassword('user2', 'qwerty'), 'Хеш зависит от логина');
+  }
+
+  function testInitWithoutSess()
+  {
+    try {
+      Auth::Init(false);
+      $this->fail('Без запущенной сессии выбрасывается исключение');
+    } catch (\CMSx\Auth\Exception $e) {
+      $this->assertEquals(Auth::ERR_SESSION, $e->getCode(), 'Код ошибки по сессии');
+    }
+
+    try {
+      Auth::Init();
+      Auth::CheckSessionStarted();
+    } catch (\CMSx\Auth\Exception $e) {
+      $this->fail('Инициализация с автостартом сессии - сессия не создалась');
+    }
   }
 }
